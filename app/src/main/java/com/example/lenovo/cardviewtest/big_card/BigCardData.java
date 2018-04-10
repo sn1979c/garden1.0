d5b55ae90afa0c7758a71130c7ccc0b047340045
sn1979c <625657112@qq.com>
2018-04-10 22:31:00 +0800

    加入了login

--- app/src/main/java/com/example/lenovo/cardviewtest/big_card/BigCardData.java
+++
@@ -1,9 +0,0 @@
-package com.example.lenovo.cardviewtest.big_card;
-
-public class BigCardData {
-    public int drawableRes;
-
-    BigCardData(int drawableRes) {
-        this.drawableRes = drawableRes;
-    }
-}
--- app/src/main/java/com/example/lenovo/cardviewtest/card_with_header/CardWithHeaderData.java
+++
@@ -1,9 +0,0 @@
-package com.example.lenovo.cardviewtest.card_with_header;
-
-public class CardWithHeaderData {
-    public int drawableRes;
-
-    CardWithHeaderData(int drawableRes) {
-        this.drawableRes = drawableRes;
-    }
-}
